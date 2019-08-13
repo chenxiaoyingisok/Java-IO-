705401bd95849dc7cbfe59e57c7d0255d3a47743
chenxiaoyingisok <1031284451@qq.com>
2019-08-13 19:44:36 +0800

    IO流相关代码和图片

---
+++ 转换流，缓冲流，打印流/demo02/ReverseStream/Demo01FileReader.java
@@ -0,0 +1,19 @@
+package com.itheima.demo02.ReverseStream;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+/*
+    FileReader:字符输入流
+        只能读取IDE默认编码格式的文件(UTF-8)
+ */
+public class Demo01FileReader {
+    public static void main(String[] args) throws IOException {
+        FileReader fr = new FileReader("day11\\我是GBK编码格式的文件.txt");
+        int len = 0;
+        while((len = fr.read())!=-1){
+            System.out.print((char)len);//���
+        }
+        fr.close();
+    }
+}
